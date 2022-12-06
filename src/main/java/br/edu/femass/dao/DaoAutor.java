55aac3bedb725577e53ec3d54e0c3c0240c52204
Tue20022 <matheusvnunes@hotmai.com>
2022-12-06 11:12:53 -0300

    Realização da tabela de Autor

--- src/main/java/br/edu/femass/dao/DaoAutor.java
+++ src/main/java/br/edu/femass/dao/DaoAutor.java
@@ -1,7 +1,6 @@
 package br.edu.femass.dao;
 
 import java.util.List;
-import javax.persistence.Query;
 import br.edu.femass.model.Autor;
 
 
--- src/main/java/br/edu/femass/gui/AutorController.java
+++ src/main/java/br/edu/femass/gui/AutorController.java
@@ -1,16 +1,18 @@
 package br.edu.femass.gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import br.edu.femass.dao.DaoAutor;
 import br.edu.femass.model.Autor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class AutorController implements Initializable {
     
@@ -20,7 +22,16 @@ public class AutorController implements Initializable {
     private TextField txtSobrenome;
     @FXML
     private TextField txtNacionalidade;
-    private ListView<Autor> LstAutores;
+    @FXML
+    private TableView<Autor> tabela = new TableView<Autor>();
+    @FXML
+    private TableColumn<Autor,Long> colId = new TableColumn<>();
+    @FXML
+    private TableColumn<Autor,String> colNome = new TableColumn<>();
+    @FXML
+    private TableColumn<Autor,String> colSobrenome = new TableColumn<>();
+    @FXML
+    private TableColumn<Autor,String> colNacionalidade = new TableColumn<>();
     private DaoAutor dao = new DaoAutor();
 
     @FXML
@@ -44,11 +55,34 @@ public class AutorController implements Initializable {
                 txtNacionalidade.getText()
         );
         dao.inserir(autor);
+        preencherTabela();
         System.out.println(autor.getId());
+    }
+
+    private void preencherTabela() {
+        List<Autor> autores = dao.buscarTodos();
+
+        ObservableList<Autor> data = FXCollections.observableArrayList(autores);
+        tabela.setItems(data);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        colId.setCellValueFactory(
+                new PropertyValueFactory<Autor, Long>("id")
+        );
+
+        colNome.setCellValueFactory(
+                new PropertyValueFactory<Autor, String>("nome")
+        );
+
+        colSobrenome.setCellValueFactory(
+                new PropertyValueFactory<Autor, String>("sobrenome")
+        );
+
+        colNacionalidade.setCellValueFactory(
+                new PropertyValueFactory<Autor, String>("nacionalidade")
+        );
+        preencherTabela();
     }
 }
